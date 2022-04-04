cc60a652dcff15d316d08eb9e6728a26bc24049c
xblurx <xblurx@protonmail.com>
2022-04-04 22:30:56 +0300

    Remove costly index array, add method that calculates index in place, clean up

--- week1/percolation.js
+++ week1/percolation.js
@@ -1,47 +1,39 @@
 const UF = require("./union-find");
 
 class Percolation {
-    indexes = [];
     sites = [];
     openSites = 0;
 
     constructor(n) {
-        function* getNSquareNumbers(n) {
-            for (let i = 1; i <= n * n; i++) {
-                yield i;
-            }
-        }
-
+        this.gridWidth = n;
         this.grid = new UF(n * n + 2);
         this.virtualTop = this.grid.id[0];
         this.virtualBottom = this.grid.id[n * n + 1];
 
-        let numbers = getNSquareNumbers(n);
-
         for (let i = 1; i <= n; i++) {
-            this.indexes[i] = [];
             this.sites[i] = [];
-        }
-
-        for (let i = 1; i <= n; i++) {
             for (let j = 1; j <= n; j++) {
-                this.indexes[i][j] = numbers.next().value;
-                this.sites[i][j] = 0;
+                this.sites[i][j] = false;
             }
         }
+    }
+
+    map2dIndexTo1d(row, col) {
+        return (row - 1) * this.gridWidth + col;
     }
 
     getNeighbors(row, col) {
         const grid = this.grid;
         const topRow = row > 1 ? row - 1 : row;
-        const rightCol = col < this.indexes.length - 1 ? col + 1 : col;
-        const bottomRow = row < this.indexes.length - 1 ? row + 1 : row;
+        const rightCol = col < this.gridWidth ? col + 1 : col;
+        const bottomRow = row < this.gridWidth ? row + 1 : row;
         const leftCol = col > 1 ? col - 1 : col;
+
         return {
-            top: grid.id[this.indexes[topRow][col]],
-            right: grid.id[this.indexes[row][rightCol]],
-            bottom: grid.id[this.indexes[bottomRow][col]],
-            left: grid.id[this.indexes[row][leftCol]],
+            top: grid.id[this.map2dIndexTo1d(topRow, col)],
+            right: grid.id[this.map2dIndexTo1d(row, rightCol)],
+            bottom: grid.id[this.map2dIndexTo1d(bottomRow, col)],
+            left: grid.id[this.map2dIndexTo1d(row, leftCol)],
             topRow,
             rightCol,
             bottomRow,
@@ -54,9 +46,9 @@ class Percolation {
 
         const { top, right, bottom, left, topRow, rightCol, bottomRow, leftCol } = this.getNeighbors(row, col);
         const grid = this.grid;
-        const site = grid.id[this.indexes[row][col]];
+        const site = grid.id[this.map2dIndexTo1d(row, col)];
 
-        this.sites[row][col] = 1;
+        this.sites[row][col] = true;
         this.openSites += 1;
 
         if (topRow == row) {
@@ -90,20 +82,20 @@ class Percolation {
     }
 
     isOpen(row, col) {
-        return this.sites[row][col] == 1;
+        return this.sites[row][col];
     }
 
     isFull(row, col) {
         // if this.grid[row][col] site have a connection to open site in a top row via a chain of neighboring open sites
-        if (this.isOpen(row, col)) {
-            const { top, right, bottom, left } = this.getNeighbors(row, col);
-            const isLeft = this.grid.find(left, this.virtualTop);
-            const isRight = this.grid.find(right, this.virtualTop);
-            const isTop = this.grid.find(top, this.virtualTop);
-            const isBottom = this.grid.find(bottom, this.virtualTop);
+        if (!this.isOpen(row, col)) return;
 
-            return isLeft || isRight || isTop || isBottom;
-        }
+        const { top, right, bottom, left } = this.getNeighbors(row, col);
+        const isLeft = this.grid.find(left, this.virtualTop);
+        const isRight = this.grid.find(right, this.virtualTop);
+        const isTop = this.grid.find(top, this.virtualTop);
+        const isBottom = this.grid.find(bottom, this.virtualTop);
+
+        return isLeft || isRight || isTop || isBottom;
     }
 
     numberOfOpenSites() {
